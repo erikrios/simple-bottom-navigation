df8acb153837c16f2bf9ca0fbbc5c41bf6209e6a
erikrios <erikriosetiawan49@gmail.com>
2020-02-13 23:08:41 +0700

    Add MenuFragment Kotlin class

---
+++ app/src/main/java/com/erikriosetiawan/simplebottomnavigation/MenuFragment.kt
@@ -0,0 +1,18 @@
+package com.erikriosetiawan.simplebottomnavigation
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.erikriosetiawan.simplebottomnavigation.databinding.FragmentMenuBinding
+
+class MenuFragment : Fragment(R.layout.fragment_menu) {
+
+    private lateinit var binding: FragmentMenuBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = DataBindingUtil.setContentView(activity as Activity, view.id)
+    }
+}
