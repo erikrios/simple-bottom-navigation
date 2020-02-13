4a88f3cd953b38204fe3f0426fedb6ea778586ff
erikrios <erikriosetiawan49@gmail.com>
2020-02-13 23:28:33 +0700

    Implements data binding, fragment, and bottom navigation view

--- app/src/main/java/com/erikriosetiawan/simplebottomnavigation/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/simplebottomnavigation/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.erikriosetiawan.simplebottomnavigation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.erikriosetiawan.simplebottomnavigation.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.bottomNavigationView.selectedItemId = R.id.menu_home
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener(menuClickListener)
+    }
+
+    private val menuClickListener = BottomNavigationView.OnNavigationItemSelectedListener {
+        val fragment: Fragment = when (it.itemId) {
+            R.id.menu_home -> HomeFragment()
+            R.id.menu_menu -> MenuFragment()
+            else -> FavoriteFragment()
+        }
+        supportFragmentManager.beginTransaction().replace(binding.frameContainer.id, fragment)
+        return@OnNavigationItemSelectedListener true
     }
 }
